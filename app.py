1bd28b153e57e7cb977de7daf97b0ed9d2a13287
Piyush Mishra <piyushmishra0207@gmail.com>
2024-12-18 14:44:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 
 # Configure SQLite database
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tasks.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/tasks.db'  # Store database in a writable directory
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
@@ -18,47 +18,6 @@ class Task(db.Model):
 def initialize_database():
     with app.app_context():
         db.create_all()
-
-@app.route('/')
-def index():
-    tasks = Task.query.all()
-    return render_template('index.html', tasks=tasks, editing=False)
-
-@app.route('/add', methods=['POST'])
-def add_task():
-    task_text = request.form.get('task')
-    if task_text:
-        new_task = Task(text=task_text)
-        db.session.add(new_task)
-        db.session.commit()
-    return redirect(url_for('index'))
-
-@app.route('/complete/<int:task_id>')
-def complete_task(task_id):
-    task = Task.query.get(task_id)
-    if task:
-        task.completed = True
-        db.session.commit()
-    return redirect(url_for('index'))
-
-@app.route('/delete/<int:task_id>')
-def delete_task(task_id):
-    task = Task.query.get(task_id)
-    if task:
-        db.session.delete(task)
-        db.session.commit()
-    return redirect(url_for('index'))
-
-@app.route('/edit/<int:task_id>', methods=['GET', 'POST'])
-def edit_task(task_id):
-    task = Task.query.get(task_id)
-    if request.method == 'POST':
-        task_text = request.form.get('task')
-        if task and task_text:
-            task.text = task_text
-            db.session.commit()
-        return redirect(url_for('index'))
-    return render_template('index.html', tasks=Task.query.all(), task=task, task_id=task_id, editing=True)
 
 if __name__ == '__main__':
     initialize_database()  # Initialize the database before starting the server
